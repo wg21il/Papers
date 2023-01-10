be0def7a6213dba471734605cbd5033229f7a2f0
Ran Regev <regev.ran@gmail.com>
2023-01-10 10:52:11 +0200

    the func (#3)
    header and tests
    
    Co-authored-by: Ran Regev <ran.regev@beyeonics.com>

--- P2022/tests/header/3way.hpp
+++ P2022/tests/header/3way.hpp
@@ -1,41 +1,52 @@
 
 
-//#include <concepts>
 #include <iterator>
-#include <compare>
 
 namespace std::ranges
 {
-
-
-template<
-    input_iterator I1, sentinel_for<I1> S1,
-    input_iterator I2, sentinel_for<I2> S2,
-    class Proj1 = identity, 
-    class Proj2 = identity,
-    class Comp = compare_three_way
->
-requires
-    three_way_comparable_with< 
-        projected<I1,Proj1>, projected<I2,Proj2> 
+    template<
+        input_iterator I1, sentinel_for<I1> S1,
+        input_iterator I2, sentinel_for<I2> S2,
+        class Comp = compare_three_way,
+        class Proj1 = identity, 
+        class Proj2 = identity
     >
-constexpr auto
-    lexicographical_compare_three_way( 
-        I1 first1, 
-        S1 last1, 
-        I2 first2, 
-        S2 last2, 
-        Comp comp = {}, 
-        Proj1 proj1 = {}, 
-        Proj2 proj2 = {}
+    constexpr auto
+        lexicographical_compare_three_way( 
+            I1 first1, 
+            S1 last1, 
+            I2 first2, 
+            S2 last2, 
+            Comp comp = {},
+            Proj1 proj1 = {}, 
+            Proj2 proj2 = {}
     ) -> common_comparison_category_t<
                 decltype(
-                        comp(proj1(*first1), proj2(*first2))), 
-                        strong_ordering
-                >
-{
-    return strong_ordering::equal;
-}
+                    comp(proj1(*first1), proj2(*first2))
+                ), 
+                strong_ordering
+         >
+    {
+        while (first1 != last1)
+        {
+            if (first2 == last2)
+            {
+                return strong_ordering::greater;
+            }
 
+            if (auto cmp = comp(proj1(*first1), proj2(*first2)); 
+                cmp != 0 ) 
+            {
+                return cmp;
+            }
+
+            ++first1;
+            ++first2;
+        }
+
+        // GCC 11 implementation with the
+        // note: See PR 94006
+        return (first2 == last2) <=> true; 
+    } 
 }
 
